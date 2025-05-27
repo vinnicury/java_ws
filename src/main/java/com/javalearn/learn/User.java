92cafbf24aa300b38e9b6666c9e70006e7725be2
Vinícius Cury <vinnicury@gmail.com>
2025-05-27 17:49:16 -0300

    ORDERS

--- src/main/java/com/javalearn/learn/User.java
+++ src/main/java/com/javalearn/learn/User.java
@@ -1,11 +1,16 @@
 package com.javalearn.learn;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +26,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
         
@@ -73,6 +82,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
