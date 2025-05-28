c332bf664d8541ef7aee1d493e22cb2c9415e9d2
Vinícius Cury <vinnicury@gmail.com>
2025-05-28 16:10:32 -0300

    one-to-one

--- src/main/java/com/javalearn/learn/config/TestConfig.java
+++ src/main/java/com/javalearn/learn/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.javalearn.learn.entities.Category;
 import com.javalearn.learn.entities.Order;
 import com.javalearn.learn.entities.OrderItem;
+import com.javalearn.learn.entities.Payment;
 import com.javalearn.learn.entities.Product;
 import com.javalearn.learn.entities.User;
 import com.javalearn.learn.enums.OrderStatus;
@@ -86,6 +87,11 @@ public class TestConfig implements CommandLineRunner {
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
 
     }
