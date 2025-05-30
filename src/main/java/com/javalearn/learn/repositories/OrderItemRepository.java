42b0edcc48aa2c65128c69885ab36951daff439f
Vinícius Cury <vinnicury@gmail.com>
2025-05-29 22:43:22 -0300

    user-insert

--- src/main/java/com/javalearn/learn/repositories/OrderItemRepository.java
+++ src/main/java/com/javalearn/learn/repositories/OrderItemRepository.java
@@ -3,7 +3,8 @@ package com.javalearn.learn.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.javalearn.learn.entities.OrderItem;
+import com.javalearn.learn.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 
 }
--- src/main/java/com/javalearn/learn/resources/UserResource.java
+++ src/main/java/com/javalearn/learn/resources/UserResource.java
@@ -1,16 +1,21 @@
 package com.javalearn.learn.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.javalearn.learn.entities.User;
 import com.javalearn.learn.services.UserService;
+
 
 @RestController
 @RequestMapping(value="/users")
@@ -32,6 +37,16 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
 
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+
+    }
+
+    
 
 
 }
